5a45e7360d5563ffc74c6d475075f5c639dad91b
rikaweb <sohrab.s.d@gmail.com>
2018-02-11 23:25:33 -0500

    Update Soen6431Menu.java

--- violetproduct-swing/src/main/java/com/horstmann/violet/application/menu/Soen6431Menu.java
+++ violetproduct-swing/src/main/java/com/horstmann/violet/application/menu/Soen6431Menu.java
@@ -5,7 +5,7 @@ import com.horstmann.violet.application.help.ShortcutDialog;
 import com.horstmann.violet.framework.injection.resources.ResourceBundleInjector;
 import com.horstmann.violet.framework.injection.resources.annotation.ResourceBundleBean;
 import com.horstmann.violet.product.diagram.abstracts.edge.IEdge;
-import com.horstmann.violet.product.diagram.abstracts.node.INode;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,47 +29,47 @@ public class Soen6431Menu extends JMenu {
     {
         feature1Item.addActionListener(new ActionListener()
         {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	Collection<INode> allActiveNodes = mainFrame.getActiveWorkspace().getGraphFile().getGraph().getAllNodes();
-                for (INode currNode : allActiveNodes) {
-                    Collection<IEdge> allActiveEdgeForSpecificNode = currNode.getGraph().getAllEdges();
-                    int count = 0;
-                    for (IEdge currEdge : allActiveEdgeForSpecificNode) {
-                        if (currEdge.getStartNode().equals(currNode) && currEdge.getEndNode().equals(currNode)) {
-                            {
-                                count++;
-                                if (count > 1) {
-                                    mainFrame.getDialogFactory().showErrorDialog( "Multi realation to itself Occured");
-                                    return;
-                                }
-                            }
-                        }
-                    }
-                }
-             }            
-        } );
-        
+        	 @Override
+             public void actionPerformed(ActionEvent e)
+             {
+                 Collection<IEdge> temp2 = mainFrame.getActiveWorkspace().getGraphFile().getGraph().getAllEdges();
+                 ArrayList<IEdge> temp_arraylist2 = new ArrayList<IEdge>(temp2);
+                 for(int i=0;i<temp_arraylist2.size();i++) {
+                     IEdge itemp2 = temp_arraylist2.get(i);
+
+
+                     if (itemp2.getStartNode().equals(itemp2.getEndNode())) {
+                         System.out.println("Class showes relation to itself");
+                         mainFrame.getDialogFactory().showErrorDialog("Class showes relation to itself");
+                         return;
+                     }
+                 }
+             }
+        });
+
         this.add(feature1Item);
 
         feature2Item.addActionListener(new ActionListener()
         {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                Collection<IEdge> temp2 = mainFrame.getActiveWorkspace().getGraphFile().getGraph().getAllEdges();
-                ArrayList<IEdge> temp_arraylist2 = new ArrayList<IEdge>(temp2);
-                for(int i=0;i<temp_arraylist2.size();i++) {
-                    IEdge itemp2 = temp_arraylist2.get(i);
+        	 @Override
+             public void actionPerformed(ActionEvent e) {
+                 Collection<IEdge> temp = mainFrame.getActiveWorkspace().getGraphFile().getGraph().getAllEdges();
+                 ArrayList<IEdge> temp_arraylist = new ArrayList(temp);
+                 for (int i = 0; i < temp_arraylist.size(); i++) {
+                     for (int j = 0; j < temp_arraylist.size(); j++) {
+                         IEdge itemp = temp_arraylist.get(i);
+                         IEdge jtemp = temp_arraylist.get(j);
 
-
-                    if (itemp2.getStartNode().equals(itemp2.getEndNode())) {
-                        System.out.println("Class showes relation to itself");
-                        mainFrame.getDialogFactory().showErrorDialog("Class showes relation to itself");
-                        return;
-                    }
-                }
-            }
+                         if (itemp.getStartNode().equals(jtemp.getEndNode()) &&
+                         		itemp.getEndNode().equals(jtemp.getStartNode())) {
+                             System.out.println("Bidirection occuring");
+                             mainFrame.getDialogFactory().showErrorDialog("BidirectionalOccuring");
+                             return;
+                         }
+                     }
+                 }
+             }
+           
         });
 
         this.add(feature2Item);
