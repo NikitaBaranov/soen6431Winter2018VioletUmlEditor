65c56148aa1debdf59987b679fcc08cdbf9101b8
rikaweb <sohrab.s.d@gmail.com>
2018-04-10 09:49:04 -0400

    fix feature 1_sohrab

--- violetproduct-swing/src/test/java/com/horstmann/violet/application/menu/Soen6431Test.java
+++ violetproduct-swing/src/test/java/com/horstmann/violet/application/menu/Soen6431Test.java
@@ -16,6 +16,54 @@ import static org.junit.Assert.assertTrue;
 public class Soen6431Test {
 
     @Test
+    public void feature1_validNdes_correctmessage() {
+        ClassNode node1 = new ClassNode();
+        SingleLineText name1 = new SingleLineText();
+        name1.setText("Node 1");
+        node1.setName(name1);
+
+        ClassNode node2 = new ClassNode();
+        SingleLineText name2 = new SingleLineText();
+        name2.setText("Node 2");
+        node2.setName(name2);
+
+        ClassNode node3 = new ClassNode();
+        SingleLineText name3 = new SingleLineText();
+        name3.setText("Node 3");
+        node3.setName(name3);
+
+        DependencyEdge dependencyEdge1 = new DependencyEdge();
+        DependencyEdge dependencyEdge2 = new DependencyEdge();
+        DependencyEdge dependencyEdge3 = new DependencyEdge();
+        DependencyEdge dependencyEdge4 = new DependencyEdge();
+        DependencyEdge dependencyEdge5 = new DependencyEdge();
+
+        ClassDiagramGraph classDiagramGraph1 = new ClassDiagramGraph();
+        classDiagramGraph1.addNode(node1, new Point2D.Double(100, 100));
+        classDiagramGraph1.addNode(node2, new Point2D.Double(100, 400));
+        classDiagramGraph1.addNode(node3, new Point2D.Double(400, 100));
+
+        classDiagramGraph1.connect(dependencyEdge1, node1, new Point2D.Double(100, 100), node1, new Point2D.Double(100, 400), new Point2D[]{});
+        classDiagramGraph1.connect(dependencyEdge4, node1, new Point2D.Double(100, 100), node1, new Point2D.Double(100, 100), new Point2D[]{});
+        classDiagramGraph1.connect(dependencyEdge4, node1, new Point2D.Double(400, 100), node1, new Point2D.Double(100, 400), new Point2D[]{});
+        classDiagramGraph1.connect(dependencyEdge5, node2, new Point2D.Double(400, 100), node2, new Point2D.Double(100, 400), new Point2D[]{});
+        classDiagramGraph1.connect(dependencyEdge5, node2, new Point2D.Double(400, 100), node2, new Point2D.Double(100, 400), new Point2D[]{});
+        
+        assertTrue(Soen6431.feature1(classDiagramGraph1));//test case 1
+
+        ClassDiagramGraph classDiagramGraph2 = new ClassDiagramGraph();
+        classDiagramGraph2.addNode(node1, new Point2D.Double(100, 100));
+        classDiagramGraph2.addNode(node2, new Point2D.Double(100, 400));
+        classDiagramGraph2.addNode(node3, new Point2D.Double(400, 100));
+
+        classDiagramGraph2.connect(dependencyEdge1, node1, new Point2D.Double(100, 100), node2, new Point2D.Double(100, 400), new Point2D[]{});
+        classDiagramGraph2.connect(dependencyEdge2, node2, new Point2D.Double(100, 400), node3, new Point2D.Double(400, 100), new Point2D[]{});
+        classDiagramGraph2.connect(dependencyEdge3, node3, new Point2D.Double(400, 100), node1, new Point2D.Double(100, 400), new Point2D[]{});
+
+        assertFalse(Soen6431.feature1(classDiagramGraph2));//test case 2
+    }
+
+    @Test
     public void feature2_validNdes_correctmessage() {
         ClassNode node1 = new ClassNode();
         SingleLineText name1 = new SingleLineText();
